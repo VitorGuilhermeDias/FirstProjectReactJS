a7448bcd84bdfef67dbd9f29589a190f663821d5
Vitor Dias <vitor.dias@cmcontrol.com>
2023-10-24 16:55:09 -0300

    feat: Inclusão do Json

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,12 +1,17 @@
 import styles from "./Select.module.css";
 
-function Select({text, name, options, handleOnChange, value }) {
+function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-     <select name={name} id={name}> 
+      <select name={name} id={name}>
         <option> Selecione uma opção </option>
-    </select>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
